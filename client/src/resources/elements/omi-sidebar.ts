983efe6daa74ed2233230dc8241a76cd89cb6586
Nikita Lobach-Zhuchenko <lobach-zhuchenko@yandex.ru>
2020-02-01 21:39:31 +0300

    semantic added

---
+++ client/src/resources/elements/omi-sidebar.ts
@@ -0,0 +1,9 @@
+import {bindable} from 'aurelia-framework';
+
+export class OmiSidebar {
+  @bindable value;
+
+  valueChanged(newValue, oldValue) {
+    //
+  }
+}
---
+++ client/src/resources/elements/test.ts
@@ -0,0 +1,9 @@
+import {bindable} from 'aurelia-framework';
+
+export class Test {
+  @bindable value;
+
+  valueChanged(newValue, oldValue) {
+    //
+  }
+}
